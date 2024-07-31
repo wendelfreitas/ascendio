504f27ca256390e555b8854348b66670fb9a3a08
Wendel Freitas <wendelfreitasbranco@gmail.com>
2024-07-31 20:02:45 -0300

    chore: update seo props

--- apps/docs/theme.config.jsx
+++ apps/docs/theme.config.jsx
@@ -1,9 +1,20 @@
 export default {
   useNextSeoProps() {
     return {
-      titleTemplate: '%s - Ascendio',
+      titleTemplate: 'Ascendio - %s',
     };
   },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Ascendio" />
+
+      <meta
+        property="og:description"
+        content="Designed to simplify the initial setup of your turborepo project. Ideal for indie developers or people looking to create a micro SaaS using most amazing packages such as Nextjs, Vite, shadcn-ui, Jest, Storybook and more."
+      />
+    </>
+  ),
   project: {
     link: 'https://ascendio.dev',
   },
